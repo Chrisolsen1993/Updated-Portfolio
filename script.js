facda92790f59d8dd0b08c896419db3537046827
Olsen <chrisolsen1993@gmail.com>
2021-12-17 13:04:37 -0500

    working on the script

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var string = "Olsen Ogouchi";
+var str = string.split("");
+var el = document.getElementsByClassName('home-content-2');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
+
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
@@ -18,8 +26,9 @@ $(".menu-btn").click(function(){
     $(".nav-link").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
 })
-// owl project script
-
-
 })
+
+
+
+
     
